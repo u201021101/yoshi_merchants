51af46b1e787dd188b29980849b57977e8b222a6
Carlos Sanchez <carlos@multiplica.com>
2017-09-10 00:49:54 -0500

    Create of welcome

--- Merchants/app/src/main/java/com/yoshi/merchants/activities/MainActivity.java
+++ Merchants/app/src/main/java/com/yoshi/merchants/activities/MainActivity.java
@@ -1,13 +1,16 @@
-package com.yoshi.merchants;
+package com.yoshi.merchants.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.yoshi.merchants.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +21,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        AppCompatButton logOutAppCompatButton= (AppCompatButton) findViewById(R.id.logOutAppCompatButton);
+        logOutAppCompatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "log out", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        AppCompatButton logInAppCompatButton= (AppCompatButton) findViewById(R.id.logInAppCompatButton);
+        logInAppCompatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "log in", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
